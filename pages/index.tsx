d8549c7feb10e74e1f86570be15896932538d4bb
Tomas Birbe <tomas.birbe@gmail.com>
2022-03-02 11:48:56 -0300

    Dev: Add custom theme

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,15 @@
+import { Container } from '@chakra-ui/react';
 import Head from 'next/head';
 
 export default function Home() {
   return (
-    <div>
+    <Container bg="red.500">
       <Head>
         <title>Blog de tomas birbe</title>
         <meta content="Blog personal de Tomas Birbe" name="description" />
         <link href="/favicon.ico" rel="icon" />
       </Head>
       <h1>Hola!</h1>
-    </div>
+    </Container>
   );
 }

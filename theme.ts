1d7870696aa75f36854c244c966c9fff6178cccc
Tomas Birbe <tomas.birbe@gmail.com>
2022-03-02 11:48:40 -0300

    Dev: Add theme and custom reset of css

---
+++ theme.ts
@@ -0,0 +1,42 @@
+import { extendTheme } from '@chakra-ui/react';
+
+export const theme = extendTheme({
+  styles: {
+    global: {
+      '*, *::before, *::after': {
+        boxSizing: 'border-box',
+      },
+
+      '*': {
+        margin: '0',
+      },
+
+      'html, body': {
+        height: '100%',
+      },
+
+      body: {
+        lineHeight: 1.5,
+        webkitFontSmoothing: 'antialiased',
+      },
+
+      'img, picture, video, canvas, svg': {
+        display: 'block',
+        maxInlineSize: '100%',
+      },
+
+      'input, button, textarea, select ': {
+        font: 'inherit',
+      },
+
+      'p, h1, h2, h3, h4, h5, h6': {
+        overflowWrap: 'break-word',
+      },
+
+      '#root, #__next': {
+        isolation: 'isolate',
+        height: '100%',
+      },
+    },
+  },
+});
